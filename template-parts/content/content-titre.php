0669b3b8235c32d484ae35d98bef1086b2e97906
Zea <e1561174@cmaisonneuve.qc.ca>
2019-03-01 19:49:06 -0500

    debug mixins 1

--- template-parts/content/content-titre.php
+++ template-parts/content/content-titre.php
@@ -9,18 +9,10 @@
  * @since 1.0.0
  */
 
+		the_title( sprintf( '<h2  class="entry-title"><a style="%c" href="%s" rel="bookmark">', 'pink', esc_url( get_permalink() ) ), '</a></h2>' );
+
+
 		
-		if( get_the_title()[4] == 1){
-			the_title( sprintf( '<h2  class="entry-title"><a style="color:pink" href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-		}
-
-		if( get_the_title()[4] == 2){
-			the_title( sprintf( '<h2  class="entry-title"><a style="color:purple" href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-		}
-
-		if( get_the_title()[4] == 2){
-			the_title( sprintf( '<h2  class="entry-title"><a style="color:bleu" href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-		}
 
 
 
