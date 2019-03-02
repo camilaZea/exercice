c573b71a31909a0c0a2008fee83d8dddbe3e5c40
Zea <e1561174@cmaisonneuve.qc.ca>
2019-03-01 20:07:28 -0500

    css

--- template-parts/content/content-titre.php
+++ template-parts/content/content-titre.php
@@ -9,7 +9,28 @@
  * @since 1.0.0
  */
 
-		the_title( sprintf( '<h2  class="entry-title"><a style="%c" href="%s" rel="bookmark">', 'pink', esc_url( get_permalink() ) ), '</a></h2>' );
+	//$a = '#ff69b4';
+	
+	if (get_the_title()[4] == 1){
+		$couleur = '#b4ff69';
+	};
+	if (get_the_title()[4] == 2){
+		$couleur = '#cc66ff';
+	};
+	if (get_the_title()[4] == 3){
+		$couleur = '#6666ff';
+	};
+	if (get_the_title()[4] == 4){
+		$couleur = '#ffff99';
+	};
+	if (get_the_title()[4] == 5){
+		$couleur = '#00cc00';
+	};
+	if (get_the_title()[4] == 6){
+		$couleur = '#003366';
+	};
+
+ 	the_title( sprintf( '<h2  class="entry-title"><a style="color:%s" href="%s" rel="bookmark">', $couleur , esc_url( get_permalink() ) ), '</a></h2>' );
 
 
 		
